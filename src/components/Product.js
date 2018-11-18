b84f51842d3dee9ddb390b9355f3a2490a94d962
redday059 <redday059@gmail.com>
2018-11-18 20:18:39 +0100

    clickable product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,28 +2,89 @@ import React from 'react'
 import {withRouter} from "react-router-dom";
 import {compose} from "redux";
 import {connect} from "react-redux";
+import get from "lodash.get";
+
+const ProductImage = ({thumbs, originals, activeId, createOnThumbClick}) => (
+  <div>
+    <img className='active' src={originals[activeId]} />
+    <div className='thumbs'>
+      { thumbs.map(
+        (thumbUrl, id) =>
+          <img
+            className={ activeId === id ? 'active' : 'blur'}
+            key={id}
+            onClick={createOnThumbClick(id)}
+            src={thumbUrl}
+          />
+      )}
+    </div>
+  </div>
+)
+
+const ProductDescription = ({title, specification, price}) => {
+  return <ul>
+    <li>{title}</li>
+    <li>{specification}</li>
+    <li>{price}</li>
+  </ul>
+}
 
 class Product extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      activeImageId: null,
     }
   }
 
+  static getDerivedStateFromProps(props, state) {
+    if (state.activeImageId !== null) {
+      return {}
+    }
+
+    if (!get(props, 'product.images.length')) {
+      return {}
+    }
+
+    return {
+      activeImageId: 0
+    };
+  }
+
+  createOnThumbClick = id => e => {
+    console.log('click', id)
+    this.setState({
+      activeImageId: id
+    })
+  }
 
   render() {
     if (!this.props.product){
       return <div>No results found.</div>;
     }
 
-    return 'from product'
+    const {imgThumbs, imgOriginals, images, ...rest} = this.props.product;
+
+    return (
+      <>
+        {this.state.activeImageId !== null ?
+          <ProductImage
+            thumbs={imgThumbs}
+            originals={imgOriginals}
+            activeId={this.state.activeImageId}
+            createOnThumbClick={this.createOnThumbClick}/> :
+          null
+        }
+        <ProductDescription {...rest} />
+      </>
+    )
   }
 }
 
 const mapStateToProps = (state, props) => (
   {
-    product: state.products[props.match.params.id]
+    product: state.products[props.match.params.id],
   }
 )
 
--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,12 +1,17 @@
 import { FETCH_PRODUCTS_TYPE } from "../actions";
+import get from "lodash.get";
 
 export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_PRODUCTS_TYPE: {
       const nextState = {...state};
       action.payload.data.forEach(
-        product => { nextState[product.id] = product }
-      )
+        product => {
+          nextState[product.id] = product;
+          product.imgOriginals = product.images.map(img => img.original)
+          product.imgThumbs = product.images.map(img => img.thumb)
+        }
+      );
       return nextState;
     }
     default:
