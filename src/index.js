a4e7278333bbe77774a502b95e9c888482f26f9a
redday059 <redday059@gmail.com>
2018-11-18 16:23:19 +0100

    adds basic structure, routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+
+import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+
+import './index.css';
+
+const createStoreWithMiddleware = createStore;
+
+ReactDOM.render(
+  <Provider store={
+    createStoreWithMiddleware(reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() /*for redux chrome-extention*/ )
+  }>
+    <Router>
+      <App>
+        <Route path='/' exact component={ProductList}/>
+        <Route path='/product/:id' component={Product}/>
+      </App>
+    </Router>
+  </Provider>
+  , document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
